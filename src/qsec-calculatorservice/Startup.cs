8789a7bcb79392ead68ff50e552a5c8a8e400e99
Ronny Hansen <ronnyha@microsoft.com>
2018-11-13 11:44:48 +0100

    Implement Web API help pages with Swagger / OpenAPI #4

--- src/qsec-calculatorservice/Startup.cs
+++ src/qsec-calculatorservice/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace qsec_calculatorservice
 {
@@ -26,6 +27,10 @@ namespace qsec_calculatorservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Math API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,17 @@ namespace qsec_calculatorservice
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Math API V1");
+            });
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
